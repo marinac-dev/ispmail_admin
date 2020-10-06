575d36734a95e5fe99bf211d5545e5a8fec3f3e0
theMarinac <nikola.djordjevic4@pmf.edu.rs>
2020-10-06 02:52:38 +0200

    Increase password strength

--- lib/ispmail_admin/accounts/user.ex
+++ lib/ispmail_admin/accounts/user.ex
@@ -44,9 +44,9 @@ defmodule IspmailAdmin.Accounts.User do
     changeset
     |> validate_required([:password])
     |> validate_length(:password, min: 12, max: 80)
-    # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
-    # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
-    # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
+    |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
+    |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
+    |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
     |> prepare_changes(&hash_password/1)
   end
 
